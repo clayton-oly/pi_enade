4add4ccfcaee1d74c93854bf67d7d87f73d53650
Clayton Rocha <clayton.will08@gmail.com>
2024-03-29 00:54:49 -0300

    Deploy

--- obj/Debug/net8.0/ProjetoEnade.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoEnade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoEnade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5bd528ade71643fc028fceb02265e101c078f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d6cafaf82b44130a74cdd818d0b58e2b8c030d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoEnade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoEnade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
